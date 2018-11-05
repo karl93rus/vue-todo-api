0526782b15b5a3926be8fdb9c4975c2cdeb54dc9
ALEXANDER SAZONOV <alexandersazonov@MacBook-Air-ALEXANDER.local>
2018-11-05 15:00:56 +0300

    Fixed CORS issues

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ mongoose.connect('mongodb://localhost/tododb', {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+  }
+  next();
+});
 
 app.use(express.json());
 app.use('/', itemsRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
+console.log(`Listening on port: ${port}...`)
